af79b6523b6799b44565f79e7f167d30d9b5b479
jorgemathbasilio <jorge.math.basilio@gmail.com>
2026-02-13 19:02:03 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -34,7 +34,7 @@ var ptx_lunr_docs = [
   "type": "Chapter",
   "number": "3",
   "title": "The Pythagorean Theorem(s)",
-  "body": " The Pythagorean Theorem(s)   Pythagorean Theorem is most important   To this day, the theorem of Pythagoras remains the most important single theorem in the whole of mathematics.   Jacob Bronowski The Ascent of Man (p. 160)   Given the above quote, it seems only natural to start with The Pythagorean Theorem; a result about right triangles.  What is a triangle? A triangle is a planar figure formed by joining three straight lines, say, of lengths, .   A triangle.       Try it!   Can you make a triangle out of lines of the following lengths? If so, is it unique?                          Can you find a general relationship between that must be satisfied in order to guarantee a triangle always exists with these side lengths?     What is an angle? We will address this more fully shortly. For now, let's assume you know what an angle is and, specifically, a right-angle: one-half a straight angle (or, 1\/4 of a full rotation).  The Pythagorean Theorem is stated and proved in the last two propositions (a.k.a. theorems) of Book I of Euclid's Elements .   Pythagorean Theorem (Euclid Book I, Proposition 47)   In right-angled triangles the square on the side opposite the right angle equals the sum of the squares on the sides containing the right angle.    "
+  "body": " The Pythagorean Theorem(s)   Pythagorean Theorem is most important   To this day, the theorem of Pythagoras remains the most important single theorem in the whole of mathematics.   Jacob Bronowski The Ascent of Man (p. 160)   Given the above quote, it seems only natural to start with The Pythagorean Theorem; a result about right triangles.  What is a triangle? A triangle is a planar figure formed by joining three straight lines, say, of lengths, .   A triangle.       Try it!   Can you make a triangle out of lines of the following lengths? If so, is it unique?                          Can you find a general relationship between that must be satisfied in order to guarantee a triangle always exists with these side lengths?     What is an angle? We will address this more fully shortly. For now, let's assume you know what an angle is and, specifically, a right-angle: one-half a straight angle (or, 1\/4 of a full rotation).  The Pythagorean Theorem is stated and proved in the last two propositions (a.k.a. theorems) of Book I of Euclid's Elements .   Pythagorean Theorem (Euclid Book I, Proposition 47)   In right-angled triangles the square on the side opposite the right angle equals the sum of the squares on the sides containing the right angle.     Pythagorean Theorem.      "
 },
 {
   "id": "ch-pythagorean-thms-4",
@@ -71,6 +71,15 @@ var ptx_lunr_docs = [
   "number": "3.0.3",
   "title": "Pythagorean Theorem (Euclid Book I, Proposition 47).",
   "body": " Pythagorean Theorem (Euclid Book I, Proposition 47)   In right-angled triangles the square on the side opposite the right angle equals the sum of the squares on the sides containing the right angle.   "
+},
+{
+  "id": "fig-pythag_proof1_fig1",
+  "level": "2",
+  "url": "ch-pythagorean-thms.html#fig-pythag_proof1_fig1",
+  "type": "Figure",
+  "number": "3.0.4",
+  "title": "",
+  "body": " Pythagorean Theorem.     "
 },
 {
   "id": "backmatter-2",
